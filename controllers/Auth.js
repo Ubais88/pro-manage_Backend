d0647da9be64438732f58662437200239ac95b8e
Ubais88 <moubais2143@gmail.com>
2024-02-22 09:36:36 +0530

    login controller added

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -2,7 +2,6 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 require("dotenv").config();
 
-
 // Signup Controller for Registering USers
 exports.signup = async (req, res) => {
   try {
@@ -53,6 +52,69 @@ exports.signup = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "User cannot be registered. Please try again.",
+    });
+  }
+};
+
+
+// Login controller for authenticating users
+exports.login = async (req, res) => {
+  try {
+    // Get email and password from request body
+    const { email, password } = req.body;
+
+    // Check if email or password is missing
+    if (!email || !password) {
+      // Return 400 Bad Request status code with error message
+      return res.status(400).json({
+        success: false,
+        message: `Please Fill up All the Required Fields`,
+      });
+    }
+
+    // Find user with provided email
+    const user = await User.findOne({ email });
+
+    // If user not found with provided email
+    if (!user) {
+      // Return 401 Unauthorized status code with error message
+      return res.status(401).json({
+        success: false,
+        message: `User is not Registered with Us Please SignUp to Continue`,
+      });
+    }
+
+    //Compare Password
+    if (await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign(
+        {
+          email: user.email,
+          id: user._id.toString(),
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "24h",
+        }
+      );
+
+      res.status(200).json({
+        success: true,
+        token,
+        user,
+        message: `User Login Success`,
+      });
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: `Password is incorrect`,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    // Return 500 Internal Server Error status code with error message
+    return res.status(500).json({
+      success: false,
+      message: `Login Failure Please Try Again`,
     });
   }
 };
