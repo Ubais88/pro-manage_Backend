7ee9a66c25635d2d55d5de50c7c927dc20dd3d35
Ubais88 <moubais2143@gmail.com>
2024-02-22 09:31:01 +0530

    DB Connected

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
 
+app.use(express.json());
+
+const database = require("./config/database");
+
+const PORT = process.env.PORT || 4000;
+database.dbconnect();
 
 app.listen(PORT, () => {
   console.log("App listening on port", PORT);
